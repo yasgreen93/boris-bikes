762d62500ee46efbca22667e79da181e802c42b7
Shane Oston Stowe <shane@shaneoston.com>
2016-03-01 17:55:08 +0000

    adding up to exercise 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,19 @@
 # lib/dockingstation.rb
 require_relative 'Bike'
+
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    bike = Bike.new
   end
 
+  def return_bike(bike)
+    @bike = bike
+  end
+
+  def docked_bike
+    @bike
+  end
 end
