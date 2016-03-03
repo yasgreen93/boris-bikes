94c38ef263dd000cc6bf6f267ba48bf6aa9ce721
phoebe <phoebe.greig@gmail.com>
2016-03-03 16:15:03 +0000

    created more unit tests

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -6,14 +6,26 @@ describe Garage do
   let(:bike) {Bike.new} 
   let(:garage) {Garage.new}
 
-	it 'unloads all the bikes from the van' do
-		6.times {station.dock(Bike.new)}
+  	before(:each) do
+  		6.times {station.dock(Bike.new)}
 		station.bikes[0].report_broken
 		station.bikes[3].report_broken
 		station.bikes[4].report_broken
 		van.collect_broken(station)
 		garage.unload(van)
+	end
+
+	it 'unloads all the bikes from the van into garage' do
+		# 6.times {station.dock(Bike.new)}
+		# station.bikes[0].report_broken
+		# station.bikes[3].report_broken
+		# station.bikes[4].report_broken
+		# van.collect_broken(station)
+		# garage.unload(van)
 		expect(garage.home.size).to eq 3
 	end
-	
+
+	it 'empties the van' do
+		expect(van.storage.size).to eq 0
+	end	
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,20 +5,18 @@ describe 'Van' do
   let(:station) {DockingStation.new}
   let(:bike) {Bike.new}
 
-
-  it 'can collect a bike' do
-    station.dock(bike)
-    station.bikes[0].report_broken
-    van.collect_broken(station)
-    expect(van.storage.size).to eq 1
-  end
-
-  it 'collects broken bikes only' do
+before(:each) do
     6.times {station.dock(Bike.new)}
     station.bikes[0].report_broken
     station.bikes[3].report_broken
     van.collect_broken(station)
+end
+
+  it 'collects broken bikes only' do
     expect(van.storage.size).to eq 2
   end
 
+  it 'removes broken bikes from the docking station' do 
+    expect(station.bikes.size).to eq 4
+  end
 end
